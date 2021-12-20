f87fdc3cfa1a69263cc37e8861e0a97f44739744
unknown <ben.tancock@gmail.com>
2021-12-19 19:13:30 -0500

    added basic printing method

--- java_testing_project_1/src/java_testing_project_1/HelloWorld.java
+++ java_testing_project_1/src/java_testing_project_1/HelloWorld.java
@@ -1,10 +1,23 @@
 package java_testing_project_1;
 
 public class HelloWorld {
+	public static void printMethod1(int x) {
+		for (int i = 0; i < x; i++) {
+			for(int j = 0; j < x; j++) {
+				if(j <= i) {
+					System.out.print(j);
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		printMethod1(5);
 	}
 
 }
